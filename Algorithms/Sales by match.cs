12be8406e566167ba43394f8ca70cb24a996ae7f
AngelVasilev <angavasilev@gmail.com>
2022-06-08 19:46:10 +0300

    Create Sales by match.cs

---
+++ Algorithms/Sales by match.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        var socks = new Dictionary<int, int>();
+        for(var i = 0; i < ar.Count; i++)
+        {
+            if(!socks.ContainsKey(ar[i]))
+            {
+                socks[ar[i]] = 0;
+            }
+            
+            socks[ar[i]]++;
+        }
+        
+        var count = 0;
+        foreach(var socksCount in socks.Values)
+        {
+            count += socksCount / 2;
+        }
+        
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
